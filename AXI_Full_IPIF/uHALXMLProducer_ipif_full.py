dc05f2faa60a60576dd3296a719bfb17112376b5
Joe Pastika <pastika@cern.ch>
2021-05-18 15:13:44 -0500

    Let uHALXMLProducer_ipif_full.py do a bit of the common work for all targets

--- AXI_Full_IPIF/uHALXMLProducer_ipif_full.py
+++ AXI_Full_IPIF/uHALXMLProducer_ipif_full.py
@@ -14,7 +14,21 @@ class UHALXMLProducer(UHALXMLProducerBase):
         #get target variables 
         target_name  = self.getProperty(fragment, 'target_name')
         target_intf  = self.getProperty(fragment, 'target_intf')
+        target_label = self.getProperty(fragment, 'target_label')
+
+        #Messy ... find the target module with "label" target_lable
+        targetFragment = None
+        for key, frag in self.factory._fullFragment.items():
+            try:
+                if self.getProperty(frag, "label") == target_label:
+                    targetFragment = frag
+                    break
+            except(KeyError, TypeError):
+                pass
+
+        if targetFragment == None:
+            raise KeyError(f"'{target_label}' not found in fragement")
 
         # forward work to producer for target
         target_key = "_".join([target_name, target_intf])
-        return self.factory[target_key](fragment, xmlDir)
+        return self.factory.getImpl(target_key)(targetFragment, xmlDir, address, target_label)
