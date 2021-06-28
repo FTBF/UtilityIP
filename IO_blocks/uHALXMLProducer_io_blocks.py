fc2479e1b4b027163d5ee0758cf30ff0ea8c9870
Jon Wilson <jsw@fnal.gov>
2021-06-28 12:55:28 -0500

    Fixed number of links in uHAL XML files for IO_blocks

--- IO_blocks/uHALXMLProducer_io_blocks.py
+++ IO_blocks/uHALXMLProducer_io_blocks.py
@@ -6,7 +6,7 @@ import shutil
 IO_blocks_channel_template = '    <node id="link%(iLink)i"   address="0x%(addr)x" module="file://IO_blocks_channel.xml" description="link %(iLink)i IO control"/>'
 
 IO_blocks_template = """<node>
-      <node id="global"   address="0x0" module="file://IO_blocks_global.xml" description="Global registers for IO control" />
+    <node id="global"   address="0x0" module="file://IO_blocks_global.xml" description="Global registers for IO control" />
 %(links)s
 </node>
 """
@@ -33,7 +33,7 @@ class UHALXMLProducer(UHALXMLProducerBase):
 
         topXML = "%s.xml"%label
         with open(os.path.join(xmlDir, "modules", topXML), "w") as f:
-            channels = [IO_blocks_channel_template%{"iLink":iLink, "addr":(iLink+1)*nreg} for iLink in range(nchip)]
+            channels = [IO_blocks_channel_template%{"iLink":iLink, "addr":(iLink+1)*nreg} for iLink in range(nchip-1)]
             f.write(IO_blocks_template%{"links": "\n".join(channels)})
 
         return top_level_node_template%{"label": label, "xml": topXML, "addr": address}
