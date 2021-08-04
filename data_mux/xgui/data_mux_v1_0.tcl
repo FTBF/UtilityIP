61513b515c39f6090c609a6c870e40b33a3200e2
Jon Wilson <jsw@fnal.gov>
2021-08-04 15:08:38 -0500

    Updated data_mux to use shared IPIF components

--- data_mux/xgui/data_mux_v1_0.tcl
+++ data_mux/xgui/data_mux_v1_0.tcl
@@ -6,6 +6,7 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "DATA_WIDTH" -parent ${Page_0}
   ipgui::add_param $IPINST -name "N_INPUTS" -parent ${Page_0}
   ipgui::add_param $IPINST -name "OUTPUT_REVERSE_BITS" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "INCLUDE_SYNCHRONIZER" -parent ${Page_0}
 
 
 }
@@ -34,6 +35,15 @@ proc update_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
 
 proc validate_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
 	# Procedure called to validate DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.INCLUDE_SYNCHRONIZER { PARAM_VALUE.INCLUDE_SYNCHRONIZER } {
+	# Procedure called to update INCLUDE_SYNCHRONIZER when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.INCLUDE_SYNCHRONIZER { PARAM_VALUE.INCLUDE_SYNCHRONIZER } {
+	# Procedure called to validate INCLUDE_SYNCHRONIZER
 	return true
 }
 
@@ -68,6 +78,11 @@ proc validate_PARAM_VALUE.OUTPUT_REVERSE_BITS { PARAM_VALUE.OUTPUT_REVERSE_BITS 
 proc update_MODELPARAM_VALUE.DATA_WIDTH { MODELPARAM_VALUE.DATA_WIDTH PARAM_VALUE.DATA_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.DATA_WIDTH}] ${MODELPARAM_VALUE.DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.INCLUDE_SYNCHRONIZER { MODELPARAM_VALUE.INCLUDE_SYNCHRONIZER PARAM_VALUE.INCLUDE_SYNCHRONIZER } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.INCLUDE_SYNCHRONIZER}] ${MODELPARAM_VALUE.INCLUDE_SYNCHRONIZER}
 }
 
 proc update_MODELPARAM_VALUE.N_INPUTS { MODELPARAM_VALUE.N_INPUTS PARAM_VALUE.N_INPUTS } {
