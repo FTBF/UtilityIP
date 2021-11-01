cba18ba17963a41a13ccbed5f4d795d08130ef63
Jon Wilson <jsw@fnal.gov>
2021-11-01 16:31:06 -0500

    Fixed handling of N_STREAMS parameter in stream_bit_reverse

--- stream_bit_reverse/xgui/stream_bit_reverse_v1_0.tcl
+++ stream_bit_reverse/xgui/stream_bit_reverse_v1_0.tcl
@@ -3,7 +3,8 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "DATA_WIDTH" -parent ${Page_0}
+  set DATA_WIDTH [ipgui::add_param $IPINST -name "DATA_WIDTH" -parent ${Page_0}]
+  set_property tooltip {Width in bits of each sub-stream contained within the input stream} ${DATA_WIDTH}
   set N_STREAMS [ipgui::add_param $IPINST -name "N_STREAMS" -parent ${Page_0}]
   set_property tooltip {Number of sub-streams contained within the input stream} ${N_STREAMS}
 
@@ -32,5 +33,10 @@ proc validate_PARAM_VALUE.N_STREAMS { PARAM_VALUE.N_STREAMS } {
 proc update_MODELPARAM_VALUE.DATA_WIDTH { MODELPARAM_VALUE.DATA_WIDTH PARAM_VALUE.DATA_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.DATA_WIDTH}] ${MODELPARAM_VALUE.DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.N_STREAMS { MODELPARAM_VALUE.N_STREAMS PARAM_VALUE.N_STREAMS } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.N_STREAMS}] ${MODELPARAM_VALUE.N_STREAMS}
 }
 
