1d089ac764281a429603cd5d0f8b824757565a29
Jon Wilson <jsw@fnal.gov>
2021-12-09 14:23:45 -0600

    Added a register to allow some portion of the input to stream_compare to be masked off and not compared.

--- stream_compare/stream_compare.sv
+++ stream_compare/stream_compare.sv
@@ -34,6 +34,8 @@ module stream_compare #(
 		output logic                                  IPIF_IP2Bus_Error
 	);
 
+	localparam NLINKS = TDATA_WIDTH / 32;
+
 	typedef struct packed
 	{
 		// Register 3
@@ -44,7 +46,8 @@ module stream_compare #(
 		// Register 1
 		logic [32-1:0] word_count;
 		// Register 0
-		logic [30-1:0] padding0;
+		logic [16-1:0] active_links;
+		logic [16-2-1:0] padding0;
 		logic          latch;
 		logic          reset;
 	} param_t;
@@ -54,7 +57,7 @@ module stream_compare #(
 	param_t params_from_bus;
 	param_t params_from_IP;
 
-	localparam param_t defaults = '{default:'0};
+	localparam param_t defaults = '{default:'0, active_links: NLINKS};
 	localparam param_t self_reset = '{default:'0, latch:1'b1, reset:1'b1};
 
 	IPIF_parameterDecode #(
@@ -104,11 +107,15 @@ module stream_compare #(
 
 	logic both_valid;
 	logic both_valid_mismatch;
+	logic [TDATA_WIDTH-1:0] mask;
 	always_comb begin
 		d = q;
 
+		for(int i = 0; i < NLINKS; i++) begin
+			mask[32*i +: 32] = (i < params_to_IP.active_links) ? '1 : '0;
+		end
 		both_valid = (S_AXIS_0_TVALID && S_AXIS_1_TVALID);
-		both_valid_mismatch = both_valid && (S_AXIS_0_TDATA != S_AXIS_1_TDATA);
+		both_valid_mismatch = both_valid && ((S_AXIS_0_TDATA & mask) != (S_AXIS_1_TDATA & mask));
 
 		S_AXIS_0_TREADY = both_valid;
 		S_AXIS_1_TREADY = both_valid;
@@ -148,10 +155,12 @@ module stream_compare #(
 			params_from_IP.trigger    <= '0;
 			params_from_IP.word_count <= '0;
 			params_from_IP.err_count  <= '0;
+			params_from_IP.active_links <= '0;
 		end else begin
 			params_from_IP.reset    <= params_to_IP.reset;
 			params_from_IP.latch    <= params_to_IP.latch;
 			params_from_IP.trigger  <= params_to_IP.trigger;
+			params_from_IP.active_links <= params_to_IP.active_links;
 			if(params_to_IP.latch == 1) begin
 				params_from_IP.word_count <= q.word_count;
 				params_from_IP.err_count  <= q.err_count;
