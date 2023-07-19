74d0d0f60cf8f9e33662200ec0d381cd3bf43fde
Zoltan Gecse <zoltan.gecse@cern.ch>
2023-07-19 17:44:25 -0500

    added SERDES to trigger input

--- exttriglogic/hdl/ExtTrigTop.sv
+++ exttriglogic/hdl/ExtTrigTop.sv
@@ -6,7 +6,9 @@ module ExtTrigTop #(
 		parameter integer N_REG = 4,
 		parameter INCLUDE_AXI_SYNC = 1
 	)(
-		input  logic clock,
+		input  logic clk40,
+		input  logic clk160,
+		input  logic clk640,
 
 		input  logic                                  IPIF_clk,
 		input  logic                                  IPIF_Bus2IP_resetn,
@@ -22,7 +24,8 @@ module ExtTrigTop #(
 		output logic                                  IPIF_IP2Bus_RdAck,
 		output logic                                  IPIF_IP2Bus_Error,
 
-		input  logic [8-1:0] trig_in,
+		input  logic asyncTrigIn_P,
+		input  logic asyncTrigIn_N,
 		input  logic busyIn_P,
 		input  logic busyIn_N,
 		input  logic startRun,
@@ -41,7 +44,8 @@ module ExtTrigTop #(
 		output logic TermEnable2,
 		output logic TermEnable3,
 		output logic ledTop,
-		output logic ledBot
+		output logic ledBot,
+		output logic [7:0] trig_phase
 	);
 
 	logic [31:0] trig_in_count;
@@ -113,6 +117,27 @@ module ExtTrigTop #(
 		.params_to_bus(params_to_bus)
 	);
 
+	logic asyncTrigIn;
+	IBUFDS IBUFDS_asyncTrigIn (
+		.O(asyncTrigIn),   // 1-bit output: Buffer output
+		.I(asyncTrigIn_P),   // 1-bit input: Diff_p buffer input (connect directly to top-level port)
+		.IB(asyncTrigIn_N)  // 1-bit input: Diff_n buffer input (connect directly to top-level port)
+	);
+
+	logic asyncTrigIn0;
+//	IDDRE1 #(
+//		.DDR_CLK_EDGE("OPPOSITE_EDGE"), // IDDRE1 mode (OPPOSITE_EDGE, SAME_EDGE, SAME_EDGE_PIPELINED)
+//		.IS_CB_INVERTED(1'b1),          // Optional inversion for CB
+//		.IS_C_INVERTED(1'b0)            // Optional inversion for C
+//	) IDDRE1_asyncTrigIn (
+//		.Q1(asyncTrigIn0), // 1-bit output: Registered parallel output 1
+//		.Q2(),             // 1-bit output: Registered parallel output 2
+//		.C(clk40),         // 1-bit input: High-speed clock
+//		.CB(clk40),        // 1-bit input: Inversion of High-speed clock C
+//		.D(asyncTrigIn),   // 1-bit input: Serial Data Input
+//		.R(0)              // 1-bit input: Active-High Async Reset
+//	);
+
 	logic busyIn;
 	IBUFDS IBUFDS_busyIn (
 		.O(busyIn),   // 1-bit output: Buffer output
@@ -128,22 +153,78 @@ module ExtTrigTop #(
 	) IDDRE1_busyIn (
 		.Q1(busyIn0), // 1-bit output: Registered parallel output 1
 		.Q2(),        // 1-bit output: Registered parallel output 2
-		.C(clock),    // 1-bit input: High-speed clock
-		.CB(clock),   // 1-bit input: Inversion of High-speed clock C
+		.C(clk40),    // 1-bit input: High-speed clock
+		.CB(clk40),   // 1-bit input: Inversion of High-speed clock C
 		.D(busyIn),   // 1-bit input: Serial Data Input
 		.R(0)         // 1-bit input: Active-High Async Reset
 	);
 
-	logic asyncTrigIn0, asyncTrigIn1, syncTrig0;
+    logic [7:0] deserialized_word;
+//  ISERDESE3  : In order to incorporate this function into the design,
+//   Verilog   : the following instance declaration needs to be placed
+//  instance   : in the body of the design code.  The instance name
+// declaration : (ISERDESE3_inst) and/or the port declarations within the
+//    code     : parenthesis may be changed to properly reference and
+//             : connect this function to the design.  All inputs
+//             : and outputs must be connected.
+
+//  <-----Cut code below this line---->
+
+   // ISERDESE3: Input SERial/DESerializer
+   //            Kintex UltraScale+
+   // Xilinx HDL Language Template, version 2021.2
+
+   ISERDESE3 #(
+      .DATA_WIDTH(8),                  // Parallel data width (4,8)
+      .FIFO_ENABLE("FALSE"),          // Enables the use of the FIFO
+      .FIFO_SYNC_MODE("FALSE"),       // Always set to FALSE. TRUE is reserved for later use.
+      .IS_CLK_B_INVERTED(1'b1),       // Optional inversion for CLK_B
+      .IS_CLK_INVERTED(1'b0),         // Optional inversion for CLK
+      .IS_RST_INVERTED(1'b0),         // Optional inversion for RST
+      .SIM_DEVICE("ULTRASCALE_PLUS")  // Set the device version for simulation functionality (ULTRASCALE,
+                                      // ULTRASCALE_PLUS, ULTRASCALE_PLUS_ES1, ULTRASCALE_PLUS_ES2)
+   )
+   ISERDESE3_inst (
+      .FIFO_EMPTY(FIFO_EMPTY),           // 1-bit output: FIFO empty flag
+      .INTERNAL_DIVCLK(INTERNAL_DIVCLK), // 1-bit output: Internally divided down clock used when FIFO is
+                                         // disabled (do not connect)
+      .Q(deserialized_word),                    // 8-bit registered output
+      .CLK(clk640),                         // 1-bit input: High-speed clock
+      .CLKDIV(clk160),                   // 1-bit input: Divided Clock
+      .CLK_B(clk640),                     // 1-bit input: Inversion of High-speed clock CLK
+      .D(asyncTrigIn),                   // 1-bit input: Serial Data Input
+      .FIFO_RD_CLK(clk160),            // 1-bit input: FIFO read clock
+      .FIFO_RD_EN(1'b0),           // 1-bit input: Enables reading the FIFO when asserted
+      .RST(1'b0)                          // 1-bit input: Asynchronous Reset
+   );
+
+   // End of ISERDESE3_inst instantiation
+
+
+//	typedef logic [7:0] word;
+//	parameter delay = 256;
+//	var word fifo[delay];
+//	for(genvar i=0;i<delay-1;i++)
+//	   always @(posedge clk160)
+//	       fifo[i+1] <= fifo[i];
+	       
+    always @(posedge clk160) begin
+        trig_phase <= deserialized_word; 
+//        fifo[0] <= deserialized_word;
+//        trig_phase <= fifo[delay-1];
+    end
+
+
+	logic asyncTrigIn1, syncTrig0;
 	logic [31:0] clockCounter;
 	logic accept, dead, running;
 
 	always_comb begin
-		asyncTrigIn0 = |trig_in;
+	   asyncTrigIn0 = deserialized_word[0];
 		accept = asyncTrigIn0 && !asyncTrigIn1 && !busyIn0 && !dead && running;
 	end
 
-	always_ff  @(posedge clock) begin
+	always_ff  @(posedge clk40) begin
 		asyncTrigIn1 <= asyncTrigIn0;
 		if(asyncTrigIn0 && !asyncTrigIn1)
 			ledTop <= !ledTop;
@@ -179,8 +260,8 @@ module ExtTrigTop #(
 		.Q(syncTrigOut),            // 1-bit output: SRL Data
 		.Q31(),                     // 1-bit output: SRL Cascade Data
 		.A(params_to_IP.trigDelay), // 5-bit input: Selects SRL depth
-		.CE(1),                     // 1-bit input: Clock enable
-		.CLK(clock),                // 1-bit input: Clock
+		.CE(1),                     // 1-bit input: clk40 enable
+		.CLK(clk40),                // 1-bit input: Clock
 		.D(accept)                  // 1-bit input: SRL Data
 	);
 
@@ -191,7 +272,7 @@ module ExtTrigTop #(
 		.SRVAL(1'b0)
 	) ODDRE1_syncTrig0 (
 		.Q(syncTrig0DDR), // 1-bit output: Data output to IOB
-		.C(clock),        // 1-bit input: High-speed clock input
+		.C(clk40),        // 1-bit input: High-speed clock input
 		.D1(syncTrig0),   // 1-bit input: Parallel data input 1
 		.D2(syncTrig0),   // 1-bit input: Parallel data input 2
 		.SR(0)            // 1-bit input: Active-High Async Reset
@@ -210,7 +291,7 @@ module ExtTrigTop #(
 		.SRVAL(1'b0)
 	) ODDRE1_syncTrigOut (
 		.Q(syncTrigOutDDR), // 1-bit output: Data output to IOB
-		.C(clock),          // 1-bit input: High-speed clock input
+		.C(clk40),          // 1-bit input: High-speed clock input
 		.D1(syncTrigOut),   // 1-bit input: Parallel data input 1
 		.D2(syncTrigOut),   // 1-bit input: Parallel data input 2
 		.SR(0)              // 1-bit input: Active-High Async Reset
@@ -231,5 +312,6 @@ module ExtTrigTop #(
 		TermEnable1 <= 0;
 		TermEnable2 <= 1;
 		TermEnable3 <= 1;
-	end
+	end					
+	
 endmodule
