9be014d4c29c27862516dd1833df51f603af572b
Jon Wilson <jsw@fnal.gov>
2024-03-06 12:50:35 -0600

    Fix clkRateTool

--- clk_mon/clkRateTool.sv
+++ clk_mon/clkRateTool.sv
@@ -4,44 +4,54 @@ module clkRateTool #(
 		parameter real MEASURE_PERIOD_s = 1,
 		parameter real MEASURE_TIME_s   = 0.001
 	)(
-		input  logic reset_in,
+		input  logic reset_in, // synchronous to clk_ref
 		input  logic clk_ref,
 		input  logic clk_test,
-		output logic [COUNTER_WIDTH-1:0] value
+		(* use_dsp = "yes" *) output logic [COUNTER_WIDTH-1:0] value // value is synchronous to clk_ref
 	);
 
 	localparam integer REF_ROLLOVER = (CLK_REF_RATE_HZ * MEASURE_PERIOD_s);
 	localparam integer SAMPLE_TIME  = (CLK_REF_RATE_HZ * MEASURE_TIME_s);
 
+	(* use_dsp = "yes" *) logic [COUNTER_WIDTH-1:0] refCtr_raw = '0;
 	logic [COUNTER_WIDTH-1:0] refCtr;
-	logic [COUNTER_WIDTH-1:0] rateCtr;
+	(* use_dsp = "yes" *) logic [COUNTER_WIDTH-1:0] rateCtr;
 	logic [COUNTER_WIDTH-1:0] rateCtr_refclk;
 	logic [COUNTER_WIDTH-1:0] rateCtr_start;
 
 	//=======================================================================
 	// reference clock domain
 	//=======================================================================
+	
+	always_ff @(posedge clk_ref) begin
+		if (refCtr_raw == REF_ROLLOVER) begin
+			refCtr_raw <= '0;
+		end else begin
+			refCtr_raw <= refCtr_raw + 1'b1;
+		end
+	end
+
+	// Add a pipeline register to make Vivado happier
+	always_ff @(posedge clk_ref) begin
+		refCtr <= refCtr_raw;
+	end
 
 	always_ff @(posedge clk_ref) begin
 		if (reset_in == 1'b1) begin
-			refCtr <= 0;
 			value <= '1;
 			rateCtr_start <= '0;
 		end else begin
 			// When we start a new measurement cycle, set the ref clock
 			// counter to 0 and latch the test clock counter
 			if (refCtr == REF_ROLLOVER) begin
-				refCtr <= 0;
 				rateCtr_start <= rateCtr_refclk;
-			end else begin
-				refCtr <= refCtr + 1;
 			end
 
 			// After we're done measuring, take the value in the test clock
 			// counter and subtract the starting value of the test clock
 			// counter
 			if (refCtr == SAMPLE_TIME) begin
-				value <= rateCtr_refclk - rateCtr_refclk;
+				value <= rateCtr_refclk - rateCtr_start;
 			end
 		end
 	end
